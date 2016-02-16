7e8f0e4ff12827104e4ba26fb2a75c4cca35a754
BrownLogic <matt@brownlogic.net>
2016-02-16 16:36:05 -0600

    Added [obfuscated] database.py

---
+++ database.py
@@ -0,0 +1,14 @@
+"""
+database.py: provides information to make a database connection
+"""
+
+# Following the pattern found here:
+# http://stackoverflow.com/questions/23966164/simplest-way-to-have-my-python-program-store-retrieve-information-from-an-online
+
+login_info = {
+    "host": "localhost",
+    "user": "YOUR_USER",
+    "passwd": "YOUR_PASSWORD",
+    "db": "projectdb",
+    "port": 3306}
+
